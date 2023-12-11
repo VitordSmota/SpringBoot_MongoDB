687cda869b3af8a397694e2ad99bacb7925a6d60
VitordSmota <vitormota05@gmail.com>
2023-12-11 11:14:05 -0300

    Referenciando os posts do usuario

--- src/main/java/com/vitormota/workshopmongo/domain/User.java
+++ src/main/java/com/vitormota/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.vitormota.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -13,6 +16,8 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
 
     public User(String id, String name, String email) {
@@ -58,4 +63,8 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
+
